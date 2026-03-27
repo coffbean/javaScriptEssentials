2b06f5dd83951e967eb7c676fbbcf251f2211372
Brandon <palmer@outlook.com>
2026-03-26 20:37:27 -0400

    Menu updated with pricing

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -7,33 +7,29 @@ const dMenuIntems = document.getElementById("dessertMenuItems");
 
 // Arrays of list of items per menu type
 
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+// const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
 
-// Displaying menu type items using MAP (no looping)
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $55', 'Pasta - $45', 'Burger - $25', 'Salmon - $40'];
+const dessertMenu = ['Cake - $12', 'Ice Cream - $10', 'Pudding - $9', 'Fruit Salad - $11'];
+
+// Displaying Breakfast menu type items using MAP (no looping)
 
 const bfMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
     bfMenuIntems.innerHTML = bfMenuItemsHTML;  // Breakfast items
 
-// const mcMenuItemsHTML = mainCourseMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
-   // mcMenuIntems.innerHTML = mcMenuItemsHTML;  // Breakfast items
+// Displaying Main Course menu type items using FOREACH loop
 
-// const dMenuItemsHTML = dessertMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
-   // dMenuIntems.innerHTML = dMenuItemsHTML;  // Breakfast items
-
-
-// Displaying menu type items using FOREACH loop
-
-let mainCourseItem = '';
+let mcItem = '';
 mainCourseMenu.forEach((item, index) => {
-    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
-    mcMenuIntems.innerHTML = mainCourseItem;
+    mcItem += `<p>Item ${index + 1}: ${item}</p>`;});
+    mcMenuIntems.innerHTML = mcItem;
 
-// Displaying menu type items using FOR loop
+// Displaying Dessert menu type items using FOR loop
 
-let dessertCourseItem = '';
+let dItem = '';
 for (i=0; i<dessertMenu.length; i++) {
-    dessertCourseItem += `<p>Item ${i+1}: ${dessertMenu[i]}</p>`;
+    dItem += `<p>Item ${i+1}: ${dessertMenu[i]}</p>`;
 }
-    dMenuIntems.innerHTML = dessertCourseItem;
+    dMenuIntems.innerHTML = dItem;
+
